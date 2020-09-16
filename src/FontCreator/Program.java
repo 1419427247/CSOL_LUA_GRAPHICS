2a08fae666856399fee9a92a44de802291ffd42d
Your Name <you@example.com>
2020-09-16 12:55:10 +0800

    Update

---
+++ src/FontCreator/Program.java
@@ -0,0 +1,139 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.*;
+import java.util.HashSet;
+import java.awt.datatransfer.*;
+import javax.swing.*;
+
+class Frame extends JFrame {
+	JPanel pJPanel = new JPanel();
+	JTextField textField = new JTextField();
+	JButton jButton = new JButton("生成");
+
+	Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+	public Frame() {
+		this.setSize(800, 120);
+
+		jButton.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(null, "已复制到剪贴板", "提示", JOptionPane.INFORMATION_MESSAGE);
+				String str = textField.getText();
+
+				HashSet<Character> set = new HashSet<Character>();
+				for (Character character : str.toCharArray()) {
+					set.add(character);
+				}
+
+				StringBuffer buff = new StringBuffer();
+				Font font = new Font("黑体", -2, 14);
+				int Width = 16;
+				int Height = 16;
+
+				for (final Character ca : set) {
+					final BufferedImage image = new BufferedImage(Width, Height, BufferedImage.TYPE_BYTE_GRAY);
+					final Graphics2D g = (Graphics2D) image.getGraphics();
+					final FontMetrics metrics = g.getFontMetrics(font);
+					g.setFont(font);
+
+					final int x = (Width - metrics.stringWidth(ca.toString())) / 2;
+					final int y = ((Height - metrics.getHeight()) / 2) + metrics.getAscent();
+
+					g.drawString(ca.toString(), x, y);
+
+					final int[][] bitmap = new int[image.getHeight()][image.getWidth()];
+
+					for (int i = 0; i < image.getHeight(); i++) {
+						for (int j = 0; j < image.getWidth(); j++) {
+							bitmap[i][j] = image.getRGB(j, i) == -16777216 ? ' ' : 'O';
+						}
+					}
+					buff.append("Font[\'" + ca + "\']={");
+
+					for (int R = 0; R < bitmap.length; R++) {
+						for (int C = 0; C < bitmap[R].length; C++) {
+							if (bitmap[R][C] == 'O') {
+								int r = 0;
+								int c = 0;
+								while (R + r < bitmap.length) {
+									if (bitmap[R + r][C] != 'O') {
+										break;
+									}
+									r++;
+								}
+								while (C + c < bitmap[0].length) {
+									if (bitmap[R][C + c] != 'O') {
+										break;
+									}
+									c++;
+								}
+								if (r > c) {
+									for (int i = 0; i < r; i++) {
+										bitmap[R + i][C] = ' ';
+									}
+									buff.append(C + "," + R + "," + 1 + "," + r + ",");
+								} else {
+									for (int i = 0; i < c; i++) {
+										bitmap[R][C + i] = ' ';
+									}
+									buff.append(C + "," + R + "," + c + "," + 1 + ",");
+								}
+							}
+						}
+					}
+					buff.deleteCharAt(buff.length() - 1);
+					buff.append("}\r\n");
+				}
+
+				clipboard.setContents(new StringSelection(buff.toString()), null);
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		pJPanel.setLayout(null);
+		this.add(pJPanel);
+
+		textField.setBounds(0, 0, 800, 50);
+		jButton.setBounds(350, 50, 100, 30);
+		pJPanel.add(textField);
+		pJPanel.add(jButton);
+
+		this.setTitle("字体生成器");
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
+
+public class Program {
+	public static void main(final String[] args) {
+		Frame f = new Frame();
+	}
+}
---
+++ src/ImageCreator/Program.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.ComponentModel;
+using System.Text;
+namespace Project
+{
+    class MyForm : Form
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        public MyForm()
+        {
+            this.SetBounds(0, 0, 800, 600);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            this.Text = "iPad水晶的图形生成器,QQ:1419427247";
+            InitializeComponent();
+        }
+        PictureBox pictureBox_Preview = new PictureBox();
+        Button button_Build = new Button();
+        Button button_SelectImage = new Button();
+
+        TrackBar trackBar_Quality = new TrackBar();
+        TrackBar trackBar_Scale = new TrackBar();
+        private void InitializeComponent()
+        {
+            openFileDialog.Filter = "图片文件(*.jpg,*.gif,*.bmp,*.png)|*.jpg;*.gif;*.bmp;*.png";
+            openFileDialog.FileOk += openFileDialog_FileOK;
+
+            pictureBox_Preview.SetBounds(200, 50, 400, 400);
+            pictureBox_Preview.Paint += pictureBox_Preview_Paint;
+            this.Controls.Add(pictureBox_Preview);
+
+            button_SelectImage.SetBounds(30, 50, 80, 40);
+            button_SelectImage.Click += button_SelectImage_Click;
+            button_SelectImage.Text = "选择文件";
+            this.Controls.Add(button_SelectImage);
+
+            button_Build.SetBounds(500, 500, 100, 30);
+            button_Build.Text = "生成";
+            button_Build.Click += button_Build_Click;
+            this.Controls.Add(button_Build);
+
+            trackBar_Quality.SetBounds(30, 500, 300, 40);
+            trackBar_Quality.Maximum = 100;
+            trackBar_Quality.Minimum = 1;
+            trackBar_Quality.Value = 100;
+            this.Controls.Add(trackBar_Quality);
+
+            trackBar_Scale.SetBounds(30, 450, 300, 40);
+            trackBar_Scale.Maximum = 100;
+            trackBar_Scale.Minimum = 1;
+            trackBar_Scale.Value = 10;
+            trackBar_Scale.ValueChanged += trackBar_Scale_ValueChanged;
+            this.Controls.Add(trackBar_Scale);
+
+
+
+            Task task = new Task(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(120);
+                    try
+                    {
+                        pictureBox_Preview.Refresh();
+                    }
+                    catch (System.Exception)
+                    {
+                    }
+
+                }
+            });
+            task.Start();
+
+        }
+
+        Image image;
+
+        int dt = 0;
+        float pixivSize = 1;
+
+        private void pictureBox_Preview_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            if (image != null)
+            {
+                FrameDimension frameDimension = new FrameDimension(image.FrameDimensionsList[0]);
+                int count = image.GetFrameCount(frameDimension);
+                if (dt < count - 1)
+                    dt++;
+                else
+                    dt = 0;
+
+                image.SelectActiveFrame(frameDimension, dt);
+
+                Bitmap bitmap = new Bitmap(image,
+                image.Width * trackBar_Quality.Value / 100,
+                image.Height * trackBar_Quality.Value / 100);
+
+                for (int i = 0; i < bitmap.Width; i++)
+                    for (int j = 0; j < bitmap.Height; j++)
+                        graphics.FillRectangle(new SolidBrush(bitmap.GetPixel(i, j)), i * pixivSize, j * pixivSize, pixivSize, pixivSize);
+                bitmap.Dispose();
+            }
+            graphics.DrawRectangle(Pens.Black, 0, 0, pictureBox_Preview.Width - 1, pictureBox_Preview.Height - 1);
+        }
+
+        private void button_SelectImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void button_Build_Click(object sender, EventArgs e)
+        {
+
+            if (this.image == null)
+            {
+                System.Windows.Forms.MessageBox.Show("未打开图片");
+                return;
+            }
+            FrameDimension frameDimension = new FrameDimension(image.FrameDimensionsList[0]);
+            int count = image.GetFrameCount(frameDimension);
+
+            int boxsize = 0;
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                image.SelectActiveFrame(frameDimension, i);
+                Bitmap bitmap = new Bitmap(image,
+                image.Width * trackBar_Quality.Value / 100,
+                image.Height * trackBar_Quality.Value / 100);
+
+                Color[,] pix = new Color[bitmap.Height, bitmap.Width];
+
+                for (int j = 0; j < bitmap.Height; j++)
+                    for (int k = 0; k < bitmap.Width; k++)
+                        pix[j, k] = bitmap.GetPixel(k, j);
+
+                builder.Append("image" + count + " = {");
+                for (int R = 0; R < bitmap.Height; R++)
+                {
+                    for (int C = 0; C < bitmap.Width; C++)
+                    {
+                        if (pix[R, C] != Color.Empty && pix[R, C].A != 0)
+                        {
+                            Color temp = pix[R, C];
+                            int r = 0;
+                            int c = 0;
+                            while (R + r < bitmap.Height)
+                            {
+                                if (pix[R + r, C] != temp)
+                                    break;
+                                r++;
+                            }
+                            while (C + c < bitmap.Width)
+                            {
+                                if (pix[R, C + c] != temp)
+                                    break;
+                                c++;
+                            }
+                            int int_color = temp.R + temp.G * 256 + temp.B * 65536;
+                            if (r > c)
+                            {
+                                for (int m = 0; m < r; m++)
+                                    pix[R + m, C] = Color.Empty;
+                                builder.Append(C + "," + R + "," + 1 + "," + r + "," + int_color + ",");
+                            }
+                            else
+                            {
+                                for (int m = 0; m < c; m++)
+                                    pix[R, C + m] = Color.Empty;
+                                builder.Append(C + "," + R + "," + c + "," + 1 + "," + int_color + ",");
+                            }
+                            boxsize++;
+                        }
+                    }
+                }
+                builder.Remove(builder.Length - 1, 1);
+                builder.Append("}\r\n");
+            }
+            Clipboard.SetDataObject(builder.ToString(), true);
+            System.Windows.Forms.MessageBox.Show("以复制到剪贴板,平均花费box数量:" + boxsize / count);
+        }
+
+        private void openFileDialog_FileOK(object sender, CancelEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show(openFileDialog.FileName);
+            image = Image.FromFile(openFileDialog.FileName);
+        }
+        private void trackBar_Scale_ValueChanged(object sender, EventArgs e)
+        {
+            pixivSize = trackBar_Scale.Value / 10f;
+        }
+
+    }
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new MyForm());
+        }
+    }
+}
